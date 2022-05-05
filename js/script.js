344bf6d1310d9ce994ca64b5178d4182242bb94c
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2022-05-06 02:51:50 +0430

    implement other sections

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-'use strict';
+"use strict";
+
+function callbackFunc(entries, observer) {
+  entries.forEach((entry) => {
+    let tempEl = entry.target;
+    let tempElAnimate = entry.target.getAttribute("data-animate");
+
+    if (entry.isIntersecting) {
+      tempEl.classList.remove("scale-0_1");
+      tempEl.classList.add(tempElAnimate);
+    } else {
+      tempEl.classList.add("scale-0_1");
+      tempEl.classList.remove(tempElAnimate);
+    }
+  });
+}
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
+};
+
+let observer = new IntersectionObserver(callbackFunc, options);
+
+observer.observe(document.getElementById("steps-img-1"));
+observer.observe(document.getElementById("steps-img-2"));
+observer.observe(document.getElementById("why-img-1"));
+observer.observe(document.getElementById("why-img-2"));
