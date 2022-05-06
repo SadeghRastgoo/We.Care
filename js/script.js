92612f879fe71cfca731efc5d18444fa37a1479c
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2022-05-06 17:17:15 +0430

    responsive, fix some bugs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const openHamburgerMenuIcon = document.getElementById("open-hamburger-icon");
+const closeHamburgerMenuIcon = document.getElementById("close-hamburger-icon");
+const responsiveNavEl = document.getElementById("nav-responsive");
+const searchTagDeleteIcons = document.querySelectorAll(".search-tag__delete");
+
 function callbackFunc(entries, observer) {
   entries.forEach((entry) => {
     let tempEl = entry.target;
@@ -27,3 +32,19 @@ observer.observe(document.getElementById("steps-img-1"));
 observer.observe(document.getElementById("steps-img-2"));
 observer.observe(document.getElementById("why-img-1"));
 observer.observe(document.getElementById("why-img-2"));
+
+openHamburgerMenuIcon.addEventListener("click", function () {
+  responsiveNavEl.classList.remove("nav--responsive--hidden");
+});
+
+function closeHamburgerMenu() {
+  responsiveNavEl.classList.add("nav--responsive--hidden");
+}
+
+closeHamburgerMenuIcon.addEventListener("click", closeHamburgerMenu);
+
+searchTagDeleteIcons.forEach((el) => {
+  el.addEventListener("click", function () {
+    console.log(el.parentElement.remove());
+  });
+});
